30add7b089da4d095fa8ce6a0a165e17127d3272
Miloas <qiyu.null@gmail.com>
2024-01-12 23:42:29 +0800

    actions

---
+++ fetch_latest.py
@@ -0,0 +1,30 @@
+import re
+import urllib.request
+import json
+
+# https://docs.python.org/3/library/urllib.request.html#examples
+# https://docs.aws.amazon.com/general/latest/gr/aws-ip-ranges.html
+res = urllib.request.urlopen('https://ziglang.org/download/index.json')
+res_body = res.read()
+
+# https://docs.python.org/3/library/json.html
+data = json.loads(res_body.decode("utf-8"))
+
+aarch64_macos = data["master"]["aarch64-macos"]
+x86_64_macos = data["master"]["x86_64-macos"]
+
+
+content = open('Formula/zig.rb', "r", encoding="utf-8").read()
+content = re.sub(
+    r'arm\?\n +url \".*?\"\n +sha256 \".*?\"',
+    f'arm?\n      url \"{aarch64_macos["tarball"]}\"\n      sha256 \"{aarch64_macos["shasum"]}\"',
+    content,
+    flags=re.DOTALL
+)
+content = re.sub(
+    r'intel\?\n +url \".*?\"\n +sha256 \".*?\"',
+    f'intel?\n      url \"{x86_64_macos["tarball"]}\"\n      sha256 \"{x86_64_macos["shasum"]}\"',
+    content,
+    flags=re.DOTALL
+)
+open('Formula/zig.rb', 'w', encoding="utf-8").write(content)
