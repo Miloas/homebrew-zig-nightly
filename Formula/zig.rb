5b65ea1b72c808510a6edb91afae65e4cbd68f7f
Miloas <qiyu.null@gmail.com>
2024-01-12 15:38:13 +0800

    Update zig.rb

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -1,45 +1,25 @@
 class Zig < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
-  url "https://ziglang.org/download/0.11.0/zig-0.11.0.tar.xz"
-  sha256 "69459bc804333df077d441ef052ffa143d53012b655a51f04cfef1414c04168c"
   license "MIT"
-  head "https://github.com/ziglang/zig.git", branch: "master"
+  
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://ziglang.org/builds/zig-macos-aarch64-0.12.0-dev.2150+63de8a598.tar.xz"
+      sha256 "6a49d8ba1f889871d6b233670aa5402042a66ab0bbeb8557e25d53cb83ea8758"
 
-  bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "2045088ae2dc8f88dcb6e10a508d677a622e01c0082e9eb4def0e4629d968028"
-    sha256 cellar: :any,                 arm64_ventura:  "c86f263129502db9b998b279c79ba373a6d9e88e2e47e4492ed696a004d21980"
-    sha256 cellar: :any,                 arm64_monterey: "fbc4211c5beacb7cc1c7c36ba1db931492fb3289bbcbc2b085f0e5af6ab40659"
-    sha256 cellar: :any,                 arm64_big_sur:  "472a2c08984811317234c134d7347266ce8e30c24ef75076f397d8b50b474e3b"
-    sha256 cellar: :any,                 sonoma:         "81b7a46ac4198743b53d92e02f20215a6505feb33c753ee6618528d3fbfc3ed6"
-    sha256 cellar: :any,                 ventura:        "9adbe95444f3b648d1cd2ae2f8dc07891f7527cb1da369fd3f5db6c75ace1079"
-    sha256 cellar: :any,                 monterey:       "172f93925e39207a580e1d5a71b211415364e8756e0a02386c9e5f6be99b1ea5"
-    sha256 cellar: :any,                 big_sur:        "00002da55679b70ef280b06f67154a449876c5ab08b13cbdaa261bacca07fa74"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "6a108d8abb0c31301daab3230db5537136281309291fcba3df62817fbbb32c13"
-  end
-
-  depends_on "cmake" => :build
-  depends_on "llvm@17" => :build
-  depends_on macos: :big_sur # ziglang/zig#13313
-  depends_on "z3"
-  depends_on "zstd"
-  uses_from_macos "ncurses"
-  uses_from_macos "zlib"
-
-  fails_with gcc: "5" # LLVM is built with GCC
-
-  def install
-    cpu = case Hardware.oldest_cpu
-    when :arm_vortex_tempest then "apple_m1" # See `zig targets`.
-    else Hardware.oldest_cpu
+      def install
+        bin.install "zig"
+      end
     end
+    if Hardware::CPU.intel?
+      url "https://ziglang.org/builds/zig-macos-x86_64-0.12.0-dev.2150+63de8a598.tar.xz"
+      sha256 "d7ac7de849b843d3c95aea3285169af5b0726b927be655be46a1b1582697eac6"
 
-    args = ["-DZIG_STATIC_LLVM=ON"]
-    args << "-DZIG_TARGET_MCPU=#{cpu}" if build.bottle?
-
-    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+      def install
+        bin.install "zig"
+      end
+    end
   end
 
   test do
