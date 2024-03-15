937cdf2c313ece69550a8419624934eafdbc0259
Automated <actions@users.noreply.github.com>
2024-03-15 13:00:34 +0000

    Update zig: Fri Mar 15 13:00:34 UTC 2024

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -2,12 +2,12 @@ class Zig < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
   license "MIT"
-  version "0.12.0-dev.3303+0103c4bf7"
+  version "0.12.0-dev.3304+bc5b09469"
   
   on_macos do
     if Hardware::CPU.arm?
-      url "https://ziglang.org/builds/zig-macos-aarch64-0.12.0-dev.3303+0103c4bf7.tar.xz"
-      sha256 "74a67508cae7faf9ea82acc14c550d2b082d1ea74b9b60eec95ce2870a79fc22"
+      url "https://ziglang.org/builds/zig-macos-aarch64-0.12.0-dev.3304+bc5b09469.tar.xz"
+      sha256 "7b447b696e66f65bc685f99227bf4848c8beb4e48ecd219465fa7b7d15dee9a3"
 
       def install
         bin.install "zig"
@@ -16,8 +16,8 @@ class Zig < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://ziglang.org/builds/zig-macos-x86_64-0.12.0-dev.3303+0103c4bf7.tar.xz"
-      sha256 "db1928ac9057e3b7aed4cbfa3eb868391afc39fea67c1a90fdd8a546e5e8e4d2"
+      url "https://ziglang.org/builds/zig-macos-x86_64-0.12.0-dev.3304+bc5b09469.tar.xz"
+      sha256 "d53f6a53d7d668dc600e2d31251d233c3013f7501fe7cd68d97078dc33d3fd24"
 
       def install
         bin.install "zig"
