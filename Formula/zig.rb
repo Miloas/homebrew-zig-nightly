9b43d611c40bc075a4dfdf2c9bac1a5d864ca182
Automated <actions@users.noreply.github.com>
2024-06-20 02:31:09 +0000

    Update zig: Thu Jun 20 02:31:09 UTC 2024

--- Formula/zig.rb
+++ Formula/zig.rb
@@ -2,12 +2,12 @@ class Zig < Formula
   desc "Programming language designed for robustness, optimality, and clarity"
   homepage "https://ziglang.org/"
   license "MIT"
-  version "0.14.0-dev.66+1fdf13a14"
+  version "0.14.0-dev.91+a154d8da8"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://ziglang.org/builds/zig-macos-aarch64-0.14.0-dev.66+1fdf13a14.tar.xz"
-      sha256 "3157ba4dec8cb39a66dbeb264bf8a3cef11eb34f6759f33e13a01bd022a01a82"
+      url "https://ziglang.org/builds/zig-macos-aarch64-0.14.0-dev.91+a154d8da8.tar.xz"
+      sha256 "30b3769383eab72bd8d768ed05a487c019962f326d144dfb9e0a1c52c63ffa95"
 
       def install
         bin.install "zig"
@@ -16,8 +16,8 @@ class Zig < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://ziglang.org/builds/zig-macos-x86_64-0.14.0-dev.66+1fdf13a14.tar.xz"
-      sha256 "8f7e820d6b7907d4be836c421118f5d0ddef7fb99573fc8bed679f8a9bd04d0a"
+      url "https://ziglang.org/builds/zig-macos-x86_64-0.14.0-dev.91+a154d8da8.tar.xz"
+      sha256 "42031fbb06bd1e35985423102e2a4b1e7a907b74df685d7a619a3a2b54c29782"
 
       def install
         bin.install "zig"
@@ -30,8 +30,8 @@ class Zig < Formula
 
   on_linux do
     if Hardware::CPU.intel? #Linux intel
-      url "https://ziglang.org/builds/zig-linux-x86_64-0.14.0-dev.66+1fdf13a14.tar.xz"
-      sha256 "bc568e1de0fa14cfbb831377ed767bf94528b23253004007bfcfc193b9578271"
+      url "https://ziglang.org/builds/zig-linux-x86_64-0.14.0-dev.91+a154d8da8.tar.xz"
+      sha256 "8e9546a4fd04b445344191872878255a126590b48d5a8ec5f3891aa497acffec"
 
       def install
         bin.install "zig"
@@ -39,8 +39,8 @@ class Zig < Formula
       end
     end
     if Hardware::CPU.arm? #Linux arm
-      url "https://ziglang.org/builds/zig-linux-aarch64-0.14.0-dev.66+1fdf13a14.tar.xz"
-      sha256 "14f8865f83be28490e29977a5a31358ac60e3a632089937edcad16148bf71d01"
+      url "https://ziglang.org/builds/zig-linux-aarch64-0.14.0-dev.91+a154d8da8.tar.xz"
+      sha256 "0711b63781842252a48176b5a8b859d7661373533e3214f2989a433a58265c55"
 
       def install
         bin.install "zig"
