70a1415c5fb3ba9e9aded0b3adae86ea188532a4
Miloas <qiyu.null@gmail.com>
2025-03-10 14:17:24 +0800

    rollback

--- fetch_latest.py
+++ fetch_latest.py
@@ -14,9 +14,9 @@ x86_64_macos = data["master"]["x86_64-macos"]
 aarch64_linux = data["master"]["aarch64-linux"]
 x86_64_linux = data["master"]["x86_64-linux"]
 
-version = data["master"]["version"]
-sha = data["master"]["src"]["shasum"][:9]
-version_hash = sha
+version_match = re.search(r"macos-aarch64-(.*?)-dev\.(.*?)\.tar\.xz", aarch64_macos["tarball"])
+version = version_match.group(1)
+version_hash = version_match.group(2)
 
 content = open('Formula/zig.rb', "r", encoding="utf-8").read()
 
