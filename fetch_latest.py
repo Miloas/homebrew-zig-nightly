edb03342f406adcf3587da65aa525d7abf576b76
Miloas <0x9357@gmail.com>
2025-10-10 06:23:38 +0800

    update

--- fetch_latest.py
+++ fetch_latest.py
@@ -2,7 +2,7 @@ import re
 import urllib.request
 import json
 
-res = urllib.request.urlopen('https://ziglang.org/download/index.json')
+res = urllib.request.urlopen("https://ziglang.org/download/index.json")
 res_body = res.read()
 
 data = json.loads(res_body.decode("utf-8"))
@@ -14,47 +14,49 @@ x86_64_macos = data["master"]["x86_64-macos"]
 aarch64_linux = data["master"]["aarch64-linux"]
 x86_64_linux = data["master"]["x86_64-linux"]
 
-version_match = re.search(r"macos-aarch64-(.*?)-dev\.(.*?)\.tar\.xz", aarch64_macos["tarball"])
+version_match = re.search(
+    r"aarch64-macos-(.*?)-dev\.(.*?)\.tar\.xz", aarch64_macos["tarball"]
+)
 version = version_match.group(1)
 version_hash = version_match.group(2)
 
-content = open('Formula/zig.rb', "r", encoding="utf-8").read()
+content = open("Formula/zig.rb", "r", encoding="utf-8").read()
 
 content = re.sub(
-    r'version \".*?\"',
-    f'version \"{version}-dev.{version_hash}\"',
+    r"version \".*?\"",
+    f'version "{version}-dev.{version_hash}"',
     content,
-    flags=re.DOTALL
+    flags=re.DOTALL,
 )
 
 content = re.sub(
-    r'arm\?\n +url \".*?macos.*?\"\n +sha256 \".*?\"',
-    f'arm?\n      url \"{aarch64_macos["tarball"]}\"\n      sha256 \"{aarch64_macos["shasum"]}\"',
+    r"arm\?\n +url \".*?macos.*?\"\n +sha256 \".*?\"",
+    f'arm?\n      url "{aarch64_macos["tarball"]}"\n      sha256 "{aarch64_macos["shasum"]}"',
     content,
-    flags=re.DOTALL
+    flags=re.DOTALL,
 )
 
 content = re.sub(
-    r'intel\?\n +url \".*?macos.*?\"\n +sha256 \".*?\"',
-    f'intel?\n      url \"{x86_64_macos["tarball"]}\"\n      sha256 \"{x86_64_macos["shasum"]}\"',
+    r"intel\?\n +url \".*?macos.*?\"\n +sha256 \".*?\"",
+    f'intel?\n      url "{x86_64_macos["tarball"]}"\n      sha256 "{x86_64_macos["shasum"]}"',
     content,
-    flags=re.DOTALL
+    flags=re.DOTALL,
 )
 
 content = re.sub(
-    r'arm\? #Linux arm\n +url \".*?linux.*?\"\n +sha256 \".*?\"',
-    f'arm? #Linux arm\n      url \"{aarch64_linux["tarball"]}\"\n      sha256 \"{aarch64_linux["shasum"]}\"',
+    r"arm\? #Linux arm\n +url \".*?linux.*?\"\n +sha256 \".*?\"",
+    f'arm? #Linux arm\n      url "{aarch64_linux["tarball"]}"\n      sha256 "{aarch64_linux["shasum"]}"',
     content,
-    flags=re.DOTALL
+    flags=re.DOTALL,
 )
 
 print(content)
 
 content = re.sub(
-    r'intel\? #Linux intel\n +url \".*?linux.*?\"\n +sha256 \".*?\"',
-    f'intel? #Linux intel\n      url \"{x86_64_linux["tarball"]}\"\n      sha256 \"{x86_64_linux["shasum"]}\"',
+    r"intel\? #Linux intel\n +url \".*?linux.*?\"\n +sha256 \".*?\"",
+    f'intel? #Linux intel\n      url "{x86_64_linux["tarball"]}"\n      sha256 "{x86_64_linux["shasum"]}"',
     content,
-    flags=re.DOTALL
+    flags=re.DOTALL,
 )
 
-open('Formula/zig.rb', 'w', encoding="utf-8").write(content)
+open("Formula/zig.rb", "w", encoding="utf-8").write(content)
